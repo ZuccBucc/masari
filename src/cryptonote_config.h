91951f95fedf98185a5b0020768aaf2740589857
ZuccBucc <zuccbucc@pm.me>
2018-04-30 21:10:50 +0100

    redo genesis with v5

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -151,7 +151,7 @@ namespace config
       0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x22
     } }; // Bender's nightmare
   std::string const GENESIS_TX =
-    "01"                                                                  // version
+    "05"                                                                  // version
     "3c"                                                                  // unlock time
     "01"                                                                  // vin length
     "ff"                                                                  // vin #1
